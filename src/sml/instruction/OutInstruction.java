54e2ed0c70218be8b4f6679a768d7da591cd9ce1
Niklas Hassforther <N.Hassi@gmx.net>
2023-03-03 18:53:51 +0000

    Javadocs for the non-arithmetic instructions and removal of not needed import statements.

--- src/sml/instruction/OutInstruction.java
+++ src/sml/instruction/OutInstruction.java
@@ -3,20 +3,38 @@ package sml.instruction;
 import sml.Instruction;
 import sml.Machine;
 import sml.RegisterName;
-import sml.Registers;
 
 import java.util.Objects;
 
+/**
+ * Represents the instruction to print out the value of a register.
+ * @author Niklas Hassforther
+ * @version 1.0
+ * @since 1.0
+ */
 public class OutInstruction extends Instruction {
     private final RegisterName source;
 
+    /**
+     * The operation code of the instruction.
+     */
     public static final String OP_CODE = "out";
 
+    /**
+     * The constructor method to instantiate a new instruction.
+     * @param label - the label of the instruction. Can be null.
+     * @param source - the register to print out.
+     */
     public OutInstruction(String label, RegisterName source) {
         super(label, OP_CODE);
         this.source = source;
     }
 
+    /**
+     * Executes the instruction.
+     * @param m - The instance of the machine class on which the instruction is executed.
+     * @return If the operation was successful the NORMAL_PROGRAM_COUNTER_UPDATE will be returned.
+     */
     @Override
     public int execute(Machine m){
         System.out.println(m.getRegisters().get(source));
