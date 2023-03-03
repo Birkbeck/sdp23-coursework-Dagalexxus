659c9b5b6fa5a697fe3d0d3d835aff3603c44a2e
Niklas Hassforther <N.Hassi@gmx.net>
2023-03-03 10:24:16 +0000

    Javadocs of the InstructionFactory interface.

--- src/sml/InstructionFactory.java
+++ src/sml/InstructionFactory.java
@@ -2,6 +2,9 @@ package sml;
 
 import java.util.List;
 
+/**
+ * The interface for an InstructionFactory to be used in the Translator class.
+ */
 public interface InstructionFactory {
     public Instruction createInstruction(String opcode, List<String> parameters);
 }
