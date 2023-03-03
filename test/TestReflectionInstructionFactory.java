a4d166b37865c9d9c4d616db99952fce5214f0fa
Niklas Hassforther <N.Hassi@gmx.net>
2023-03-03 17:46:21 +0000

    Additional test for the TestReflectionInstructionFactory

--- test/TestReflectionInstructionFactory.java
+++ test/TestReflectionInstructionFactory.java
@@ -8,6 +8,7 @@ import sml.ReflectionInstructionFactory;
 import sml.Registers;
 import sml.exceptions.OpcodeNotFoundException;
 import sml.instruction.AddInstruction;
+import sml.instruction.MovInstruction;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -29,6 +30,22 @@ public class TestReflectionInstructionFactory {
         }
         catch (Exception e){
             Assertions.assertEquals(1,0);
+        }
+    }
+
+    @Test
+    void ValidTest2(){
+        Instruction mov = new MovInstruction(null, Registers.Register.EAX, 3);
+        List<String> parameters = new ArrayList<>();
+        parameters.add(null);
+        parameters.add("EAX");
+        parameters.add("3");
+
+        try {
+            Assertions.assertEquals(factory.createInstruction("mov", parameters), mov);
+        }
+        catch (Exception e){
+            Assertions.assertEquals(1,2);
         }
     }
 
